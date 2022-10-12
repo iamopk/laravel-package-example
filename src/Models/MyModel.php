e323b80a1ccacde1a3f56c55a3994c796d19513d
iam <iam@iam.iam>
2022-10-13 00:11:44 +0500

    fixed phpstan failure

--- src/Models/MyModel.php
+++ src/Models/MyModel.php
@@ -5,6 +5,12 @@ namespace Iamopk\LaravelPackageExample\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class MyModel extends Model
 {
     use HasFactory;
